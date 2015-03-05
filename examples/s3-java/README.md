53e729b3d1cb6034a463b951276d272336f96066
Clemens Siebler <clemens.siebler@gmail.com>
2015-03-05 11:44:27 +0100

    Added basic acceptence tests to qualify other versions of the AWS SDK for Java

--- examples/s3-java/README.md
+++ examples/s3-java/README.md
@@ -6,3 +6,17 @@ $ ./gradlew run
 ```
 This will automatically download Gradle and the required libraries. Then, it will compile and execute the example code.
 
+To generate an Eclipse project file, use:
+```
+$ ./gradlew eclipse
+```
+and then choose `File -> Import` in Eclipse, and import the `s3-java` folder.
+
+To run the tests (i.e., to validate other version of the used AWS SDK), run:
+```
+$ ./gradlew test
+```
+
+# Notes
+
+The provided acceptence tests are a quick shot for making sure, that basic functionality works.
---
+++ examples/s3-java/src/test/java/codeexamples/ExamplesTest.java
@@ -0,0 +1,150 @@
+package codeexamples;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.S3ClientOptions;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.Owner;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+
+public class ExamplesTest {
+
+    // Test configuration
+    final String profile = "my_profile";
+    final String address = "https://10.65.57.176:8082";
+
+    final private AWSCredentialsProvider credentials = new ProfileCredentialsProvider(profile);
+    final private S3ClientOptions options = new S3ClientOptions().withPathStyleAccess(true);
+    private AmazonS3Client s3;
+
+    // Test variables
+    final private String testBucket = "testbucket-" + System.currentTimeMillis();
+    final private String testContent = "This is my test object's content!";
+    final private String testObjectKey = "test_object";
+
+    @Before
+    public void setup() {
+        s3 = new AmazonS3Client(credentials);
+        s3.setEndpoint(address);
+        s3.setS3ClientOptions(options);
+        s3.createBucket(testBucket);
+    }
+
+    @After
+    public void cleanup() {
+        s3.deleteBucket(testBucket);
+    }
+
+    @Test
+    public void testListBuckets() {
+        List<Bucket> buckets = s3.listBuckets();
+        assertTrue(buckets.size() >= 0);
+    }
+
+    @Test
+    public void testCreateAndDeleteBucket() {
+
+        // create new bucket
+        String bucketName = "bucket-" + System.currentTimeMillis();
+        s3.createBucket(bucketName);
+
+        // make sure it is there
+        List<Bucket> buckets = s3.listBuckets();
+        List<String> bucketNames = extractBucketNames(buckets);
+        assertTrue(bucketNames.contains(bucketName));
+
+        // delete it
+        s3.deleteBucket(bucketName);
+
+        // make sure it is gone
+        buckets = s3.listBuckets();
+        bucketNames = extractBucketNames(buckets);
+        assertFalse(bucketNames.contains(bucketName));
+    }
+
+    @Test
+    public void testGetS3AccountOwner() {
+        Owner owner = s3.getS3AccountOwner();
+
+        // Makes sure it returns something
+        assertTrue(owner.getDisplayName().length() > 0);
+        assertTrue(owner.getId().length() > 24);
+    }
+
+    @Test
+    public void testCreateAndDeleteObject() throws IOException {
+        // create object
+        final ObjectMetadata metadata = new ObjectMetadata();
+        final InputStream inputStream = IOUtils.toInputStream(testContent, "UTF-8");
+        metadata.addUserMetadata("mykey1", "myvalue1");
+        metadata.addUserMetadata("mykey2", "myvalue2");
+        s3.putObject(testBucket, testObjectKey, inputStream, metadata);
+
+        // list objects and make sure it is there
+        ObjectListing objectListing = s3.listObjects(testBucket);
+        List<String> keys = extractObjectKeys(objectListing);
+        assertTrue(keys.contains(testObjectKey));
+
+        // make sure it is our object
+        S3Object object = s3.getObject(testBucket, testObjectKey);
+        List<String> retrievedContent = IOUtils.readLines(object.getObjectContent());
+        assertTrue(testContent.equals(retrievedContent.get(0)));
+
+        // make sure the metadata matches too
+        ObjectMetadata oMetadata = s3.getObjectMetadata(testBucket, testObjectKey);
+        assertEquals(2, oMetadata.getUserMetadata().keySet().size());
+        assertTrue("myvalue1".equals(oMetadata.getUserMetadata().get("mykey1")));
+        assertTrue("myvalue2".equals(oMetadata.getUserMetadata().get("mykey2")));
+
+        // delete object
+        s3.deleteObject(testBucket, testObjectKey);
+
+        // make sure it is done
+        objectListing = s3.listObjects(testBucket);
+        keys = extractObjectKeys(objectListing);
+        assertFalse(keys.contains(testObjectKey));
+    }
+
+    @Test
+    public void testListObject() {
+        ObjectListing objectListing = s3.listObjects(testBucket);
+        List<S3ObjectSummary> objects = objectListing.getObjectSummaries();
+        assertEquals(0, objects.size());
+    }
+
+    private List<String> extractBucketNames(List<Bucket> buckets) {
+        List<String> bucketNames = new ArrayList<String>();
+        for (Bucket b : buckets) {
+            bucketNames.add(b.getName());
+        }
+        return bucketNames;
+    }
+
+    private List<String> extractObjectKeys(ObjectListing objectListing) {
+        List<String> objectKeys = new ArrayList<String>();
+        List<S3ObjectSummary> objects = objectListing.getObjectSummaries();
+        for (S3ObjectSummary o : objects) {
+            objectKeys.add(o.getKey());
+        }
+        return objectKeys;
+    }
+
+}
