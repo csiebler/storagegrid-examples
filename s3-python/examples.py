51c6d7cc43f9467c3723ae03ea1579565d6c30b9
Clemens Siebler <clemens.siebler@gmail.com>
2017-02-23 11:54:04 +0100

    Updating Python examples to new features from SGWS 10.3

--- s3-python/examples.py
+++ s3-python/examples.py
@@ -7,11 +7,11 @@ session = boto3.session.Session(profile_name='my_profile')
 Do not use this in production - disabling SSL verification is discouraged!
 When using a self-signed certificate, make sure to pass it into the constructor:
 
-endpoint = 'https://sg-gw1.mycompany.com:8082'
+endpoint = 'https://s3.mycompany.com:8082'
 s3 = session.resource(service_name='s3', endpoint_url=endpoint, verify='server_cert.pem')
 '''
 
-endpoint = 'https://10.65.57.176:8082'
+endpoint = 'https://s3.mycompany.com:8082'
 s3 = session.resource(service_name='s3', endpoint_url=endpoint, verify=False)
 client = s3.meta.client
 
@@ -20,21 +20,28 @@ Bucket related operations
 '''
 
 # Create new bucket for S3 account
-s3.Bucket('my-bucket').create()
+bucket = s3.Bucket('my-bucket')
+bucket.create()
 
 # List all buckets for S3 account
 for bucket in s3.buckets.all():
     print(bucket.name)
 
+# Enable Bucket Versioning
+bucket.Versioning().enable()
+
+# Get Bucket Versioning status
+print bucket.Versioning().status
+
 # Delete bucket
-s3.Bucket('my-bucket').delete()
+bucket.delete()
 
 '''
 Object related operations
 '''
 
 # Put a new object to a bucket
-obj = s3.Object('test', 'my-key')
+obj = s3.Object('my-bucket', 'my-key')
 obj.put(Body='This is my object\'s data',
         Metadata={'customerid': '1234', 'location': 'germany'},
         ServerSideEncryption='AES256')
@@ -46,8 +53,8 @@ obj.put(Body='This is my object\'s data',
 #obj.download_file('target-file')
 
 # Copy an existing object
-copied_obj = s3.Object('test', 'my-copied-key')
-copied_obj.copy_from(CopySource='/test/my-key')
+copied_obj = s3.Object('my-bucket', 'my-copied-key')
+copied_obj.copy_from(CopySource='/my-bucket/my-key')
 
 # Get object from bucket
 response = obj.get()
@@ -56,11 +63,11 @@ metadata = response['Metadata']
 print("Data: %s // Metadata: %s" % (data, metadata))
 
 # List all objects for a bucket
-for obj in s3.Bucket('test').objects.all():
+for obj in s3.Bucket('my-bucket').objects.all():
     print(obj.key)
 
 # Generate a pre-signed URL (only possible via client, not directly via Object object)
-url = client.generate_presigned_url('get_object', {'Bucket': 'test', 'Key': 'my-key'}, ExpiresIn=3600)
+url = client.generate_presigned_url('get_object', {'Bucket': 'my-bucket', 'Key': 'my-key'}, ExpiresIn=3600)
 print("Pre-signed URL: %s" % (url))
 
 # Delete the object from its bucket
